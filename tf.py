20035a287054896dcdafd326561693e666ca6d25
Joshuaweg <joshua.weg.95@gmail.com>
2023-06-28 14:20:44 -0700

    adding files

--- tf.py
+++ tf.py
@@ -8,12 +8,18 @@ docs = []
 tfidf = TfidfVectorizer()
 with open('books.json','r') as books:
     data=json.load(books)
-    print(len(data.keys()))
+    #print(len(data.keys()))
     for t in data.keys():
         docs.append(" ".join(data[t]["tokens"]))
-        print(len(docs))
+        #print(len(docs))
     results = tfidf.fit_transform(docs)
-    print(tfidf.vocabulary_)
+    print(len(tfidf.vocabulary_))
     print(results)
-    print(results.toarray())
+    print(results.toarray()[0][:200])
+    print(results.toarray()[0][200:400])
+    print(results.toarray()[0][400:600])
+    print(results.toarray()[0][600:800])
+    print(results.toarray()[0][800:1000])
+    print(results.toarray()[0][1000:1200])
+    print(results.toarray()[0][1200:1400])
 
